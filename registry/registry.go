4475c41f36801e5e29c3272773e93aaa2797383e
sakryt <sakryt@163.com>
2024-02-20 17:33:05 +0800

    定义一个简单的服务注册中心

---
+++ registry/registry.go
@@ -0,0 +1,62 @@
+package registry
+
+import (
+	"sort"
+	"sync"
+	"time"
+)
+
+// GeeRegistry is a simple register center, provide following functions.
+// add a server and receive heartbeat to keep it alive.
+// returns all alive servers and delete dead servers sync simultaneously
+type GeeRegistry struct {
+	timeout time.Duration
+	mu      sync.Mutex // protect following
+	servers map[string]*ServerItem
+}
+
+type ServerItem struct {
+	Addr  string
+	start time.Time
+}
+
+const (
+	defaultPath    = "/_geerpc_/registry"
+	defaultTimeout = time.Minute * 5
+)
+
+// New create a registry instance with timeout setting
+func New(timeout time.Duration) *GeeRegistry {
+	return &GeeRegistry{
+		servers: make(map[string]*ServerItem),
+		timeout: timeout,
+	}
+}
+
+var DefaultGeeRegister = New(defaultTimeout)
+
+func (r *GeeRegistry) putServer(addr string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	s := r.servers[addr]
+	if s == nil {
+		r.servers[addr] = &ServerItem{Addr: addr, start: time.Now()}
+	} else {
+		s.start = time.Now() // if exists, update start time to keep alive
+	}
+}
+
+func (r *GeeRegistry) aliveServers() []string {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	var alive []string
+	for addr, s := range r.servers {
+		if r.timeout == 0 || s.start.Add(r.timeout).After(time.Now()) {
+			alive = append(alive, addr)
+		} else {
+			delete(r.servers, addr)
+		}
+	}
+	sort.Strings(alive)
+	return alive
+}
