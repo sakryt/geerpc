c58e3460c8836a9573a44a00ee341bd1b63abf2f
sakryt <sakryt@163.com>
2024-02-21 17:31:21 +0800

    通过http提供注册中心的注册和获取方法，并提供心跳方法

--- registry/registry.go
+++ registry/registry.go
@@ -1,7 +1,10 @@
 package registry
 
 import (
+	"log"
+	"net/http"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 )
@@ -59,4 +62,64 @@ func (r *GeeRegistry) aliveServers() []string {
 	}
 	sort.Strings(alive)
 	return alive
+}
+
+// Runs at /_geerpc_/registry
+func (r *GeeRegistry) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		// keep it simple, server is in req.Header
+		w.Header().Set("X-Geerpc-Servers", strings.Join(r.aliveServers(), ","))
+	case "POST":
+		// keep it simple, server is in req.Header
+		addr := req.Header.Get("X-Geerpc-Server")
+		if addr == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		r.putServer(addr)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+// HandleHTTP registers an HTTP handler for GeeRegistry messages on registryPath
+func (r *GeeRegistry) HandleHTTP(registryPath string) {
+	http.Handle(registryPath, r)
+	log.Println("rpc registry path:", registryPath)
+}
+
+func HandleHTTP() {
+	DefaultGeeRegister.HandleHTTP(defaultPath)
+}
+
+// Heartbeat send a heartbeat message every once in a while
+// it's a helper function for a server to register or send heartbeat
+func Heartbeat(registry, addr string, duration time.Duration) {
+	if duration == 0 {
+		// make sure there is enough time to send heart beat
+		// before it's removed from registry
+		duration = defaultTimeout - time.Duration(1)*time.Minute
+	}
+	var err error
+	err = sendHeartbeat(registry, addr)
+	go func() {
+		t := time.NewTicker(duration)
+		for err == nil {
+			<-t.C
+			err = sendHeartbeat(registry, addr)
+		}
+	}()
+}
+
+func sendHeartbeat(registry, addr string) error {
+	log.Println(addr, "send heart beat to registry", registry)
+	httpClient := &http.Client{}
+	req, _ := http.NewRequest("POST", registry, nil)
+	req.Header.Set("X-Geerpc-Server", addr)
+	if _, err := httpClient.Do(req); err != nil {
+		log.Println("rpc server: heart beat err:", err)
+		return err
+	}
+	return nil
 }
